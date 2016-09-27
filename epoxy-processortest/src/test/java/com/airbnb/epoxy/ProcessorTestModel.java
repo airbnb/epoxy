e593ef58fcdd4eced8abc083addfcd8dd9e786c8
Eli Hart <konakid@gmail.com>
2016-09-27 10:47:26 -0700

    Remove processor dependency on support library and EpoxyModel (#53)

--- epoxy-processortest/src/test/java/com/airbnb/epoxy/ProcessorTestModel.java
+++ epoxy-processortest/src/test/java/com/airbnb/epoxy/ProcessorTestModel.java
@@ -1,6 +1,9 @@
 package com.airbnb.epoxy;
 
-public class TestModel extends EpoxyModel<Object> {
+import com.airbnb.epoxy.EpoxyAttribute;
+import com.airbnb.epoxy.EpoxyModel;
+
+public class ProcessorTestModel extends EpoxyModel<Object> {
   @EpoxyAttribute public int publicValue;
   @EpoxyAttribute protected int protectedValue;
   @EpoxyAttribute int packagePrivateValue;
