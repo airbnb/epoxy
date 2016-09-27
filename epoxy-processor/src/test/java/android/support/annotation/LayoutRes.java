ea577668414907bc23c1cb059f6fc301273d7eaf
Eli Hart <eli.hart@airbnb.com>
2016-09-26 21:54:12 -0700

    fix processor dependencies

---
+++ epoxy-processor/src/test/java/android/support/annotation/LayoutRes.java
@@ -0,0 +1,15 @@
+package android.support.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE})
+public @interface LayoutRes {
+}
---
+++ epoxy-processor/src/test/java/com/airbnb/epoxy/EpoxyModel.java
@@ -0,0 +1,5 @@
+package com.airbnb.epoxy;
+
+public abstract class EpoxyModel<T> {
+  protected abstract int getDefaultLayout();
+}
