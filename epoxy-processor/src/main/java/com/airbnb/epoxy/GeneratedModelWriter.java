4feffe37321e35156387aaf623edf5bbfbb32af7
Eli Hart <eli.hart@airbnb.com>
2017-03-07 16:15:45 -0800

    fix javadoc

--- epoxy-processor/src/main/java/com/airbnb/epoxy/GeneratedModelWriter.java
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/GeneratedModelWriter.java
@@ -262,14 +262,13 @@ class GeneratedModelWriter {
     ParameterSpec bindListenerParam = ParameterSpec.builder(onBindListenerType, "listener").build();
 
     methods.add(MethodSpec.methodBuilder("onBind")
-        .addJavadoc(CodeBlock
-            .of("Register a listener that will be called when this model is bound to a view.\n"
+        .addJavadoc("Register a listener that will be called when this model is bound to a view.\n"
                 + "<p>\n"
                 + "The listener will contribute to this model's hashCode state per the {@link\n"
                 + "com.airbnb.epoxy.EpoxyAttribute.Option#DoNotHash} rules.\n"
                 + "<p>\n"
                 + "You may clear the listener by setting a null value, or by calling "
-                + "{@link #reset()}"))
+                + "{@link #reset()}")
         .addModifiers(PUBLIC)
         .returns(classInfo.getParameterizedGeneratedName())
         .addParameter(bindListenerParam)
@@ -299,15 +298,14 @@ class GeneratedModelWriter {
         ParameterSpec.builder(onUnbindListenerType, "listener").build();
 
     methods.add(MethodSpec.methodBuilder("onUnbind")
-        .addJavadoc(CodeBlock
-            .of("Register a listener that will be called when this model is unbound from a "
+        .addJavadoc("Register a listener that will be called when this model is unbound from a "
                 + "view.\n"
                 + "<p>\n"
                 + "The listener will contribute to this model's hashCode state per the {@link\n"
                 + "com.airbnb.epoxy.EpoxyAttribute.Option#DoNotHash} rules.\n"
                 + "<p>\n"
                 + "You may clear the listener by setting a null value, or by calling "
-                + "{@link #reset()}"))
+                + "{@link #reset()}")
         .addModifiers(PUBLIC)
         .returns(classInfo.getParameterizedGeneratedName())
         .addParameter(unbindListenerParam)
@@ -522,10 +520,9 @@ class GeneratedModelWriter {
         ParameterSpec.builder(getModelClickListenerType(helperClass), attributeName, FINAL).build();
 
     return MethodSpec.methodBuilder(attributeName)
-        .addJavadoc(CodeBlock
-            .of("Set a click listener that will provide the parent view, model, and adapter "
+        .addJavadoc("Set a click listener that will provide the parent view, model, and adapter "
                 + "position of the clicked view. This will clear the normal View.OnClickListener "
-                + "if one has been set"))
+                + "if one has been set")
         .addModifiers(PUBLIC)
         .returns(helperClass.getParameterizedGeneratedName())
         .addParameter(param)
