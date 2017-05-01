1327f9243ee507c3d041694db595a8dd7297610d
Ilya Zorin <geraltencore@gmail.com>
2017-05-01 17:30:48 +0300

    Databinding WIP

--- epoxy-processor/src/main/java/com/airbnb/epoxy/DataBindingAttributeInfo.java
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/DataBindingAttributeInfo.java
@@ -1,4 +1,22 @@
 package com.airbnb.epoxy;
 
+import com.squareup.javapoet.TypeName;
+
+import javax.lang.model.element.Element;
+
 class DataBindingAttributeInfo extends AttributeInfo {
+
+  public DataBindingAttributeInfo(DataBindingModelInfo modelInfo, Element dataBindingElement) {
+    name = dataBindingElement.getSimpleName().toString();
+    typeName = TypeName.get(dataBindingElement.asType());
+    typeMirror = dataBindingElement.asType();
+    modelName = modelInfo.getGeneratedName().simpleName();
+    modelPackageName = modelInfo.getGeneratedName().packageName();
+    useInHash = true;
+    ignoreRequireHashCode = false;
+    generateSetter = true;
+    generateGetter = true;
+    hasFinalModifier = false;
+    packagePrivate = false;
+  }
 }
--- epoxy-processor/src/main/java/com/airbnb/epoxy/DataBindingModelInfo.java
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/DataBindingModelInfo.java
@@ -1,46 +1,78 @@
 package com.airbnb.epoxy;
 
+import com.squareup.javapoet.ClassName;
+
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 
+import static com.airbnb.epoxy.ClassNames.EPOXY_DATA_BINDING_MODEL;
+import static com.airbnb.epoxy.ProcessorUtils.capitalizeFirstLetter;
 import static com.airbnb.epoxy.ProcessorUtils.getElementByName;
 
 class DataBindingModelInfo extends GeneratedModelInfo {
-  private final LayoutResource layoutResource;
-  private final String moduleName;
-  private final String dataBindingClassName;
 
-  DataBindingModelInfo(LayoutResource layoutResource, String moduleName) {
+  private final static String BINDING_SUFFIX = "Binding";
+  private final static String SET_PREFIX = "set";
+
+  private final Types typeUtils;
+  private final Elements elementUtils;
+  private final LayoutResource layoutResource;
+  private final ClassName dataBindingClassName;
+
+  DataBindingModelInfo(Types typeUtils, Elements elementUtils, LayoutResource layoutResource,
+      String moduleName) {
     this.layoutResource = layoutResource;
-    this.moduleName = moduleName;
 
     dataBindingClassName = getDataBindingClassNameForResource(layoutResource, moduleName);
+
+    this.typeUtils = typeUtils;
+    this.elementUtils = elementUtils;
+
+    superClassElement = (TypeElement) ProcessorUtils.getElementByName(EPOXY_DATA_BINDING_MODEL,
+        elementUtils, typeUtils);
+    superClassName = EPOXY_DATA_BINDING_MODEL;
+    generatedClassName = buildGeneratedModelName(dataBindingClassName);
+    parameterizedClassName = generatedClassName;
+    shouldGenerateModel = true;
+    generateFieldsForAttributes = true;
   }
 
-  void parseDataBindingClass(Types typeUtils, Elements elementUtils) {
+  void parseDataBindingClass() {
     // This databinding class won't exist until the second round of annotation processing since
     // it is generated in the first round.
     Element dataBindingClass = getElementByName(dataBindingClassName, elementUtils, typeUtils);
 
-    // TODO: Inspect the methods enclosed in the binding class to find setters. Use those setters
-    // to get the variable names and types.
-
-    //addAttribute(new DataBindingAttributeInfo());
+    for (Element element : dataBindingClass.getEnclosedElements()) {
+      if (element.getKind() == ElementKind.METHOD) {
+        ExecutableElement method = (ExecutableElement) element;
+        if (method.getSimpleName().toString().startsWith(SET_PREFIX)
+            && method.getParameters().size() == 1) {
+          Element dataBindingElement = method.getParameters().get(0);
+          addAttribute(new DataBindingAttributeInfo(this, dataBindingElement));
+        }
+      }
+    }
   }
 
-  private String getDataBindingClassNameForResource(LayoutResource layoutResource,
+  private ClassName getDataBindingClassNameForResource(LayoutResource layoutResource,
       String moduleName) {
-    // TODO
+    StringBuilder builder = new StringBuilder();
+    String[] parts = layoutResource.resourceName.split("_");
+    for (String part : parts) {
+      builder.append(capitalizeFirstLetter(part));
+    }
+    builder.append(BINDING_SUFFIX);
 
-    // From https://developer.android.com/topic/libraries/data-binding/index.html
-    // By default, a Binding class will be generated based on the name of the layout file,
-    // converting it to Pascal case and suffixing "Binding" to it. The above layout file was
-    // main_activity.xml so the generate class was MainActivityBinding.
+    return ClassName.get(moduleName, builder.toString());
+  }
 
-    // We don't support custom class names via <data class="com.example.CustomClassName">
-
-    return "";
+  private ClassName buildGeneratedModelName(ClassName className) {
+    String simpleName = className.simpleName() + "Model" + GENERATED_CLASS_NAME_SUFFIX;
+    return ClassName.get(className.packageName(), simpleName);
   }
 
   LayoutResource getLayoutResource() {
