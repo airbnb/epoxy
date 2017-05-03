9e45c9a946bb772e1ae63913cb1e5d3868cbbdd4
geralt-encore <geraltencore@gmail.com>
2017-05-03 16:12:00 +0300

    Code generation test for fully generated data binding model

---
+++ epoxy-processortest/src/test/resources/ModelWithDataBindingBinding.java
@@ -0,0 +1,126 @@
+package com.airbnb.epoxy.databinding;
+import com.airbnb.epoxy.R;
+import com.airbnb.epoxy.BR;
+import android.view.View;
+public class ModelWithDataBindingBinding extends android.databinding.ViewDataBinding  {
+
+  private static final android.databinding.ViewDataBinding.IncludedLayouts sIncludes;
+  private static final android.util.SparseIntArray sViewsWithIds;
+  static {
+    sIncludes = null;
+    sViewsWithIds = null;
+  }
+  // views
+  public final android.widget.Button button;
+  // variables
+  private java.lang.String mStringValue;
+  // values
+  // listeners
+  // Inverse Binding Event Handlers
+
+  public ModelWithDataBindingBinding(android.databinding.DataBindingComponent bindingComponent, View root) {
+    super(bindingComponent, root, 0);
+    final Object[] bindings = mapBindings(bindingComponent, root, 1, sIncludes, sViewsWithIds);
+    this.button = (android.widget.Button) bindings[0];
+    this.button.setTag(null);
+    setRootTag(root);
+    // listeners
+    invalidateAll();
+  }
+
+  @Override
+  public void invalidateAll() {
+    synchronized(this) {
+      mDirtyFlags = 0x2L;
+    }
+    requestRebind();
+  }
+
+  @Override
+  public boolean hasPendingBindings() {
+    synchronized(this) {
+      if (mDirtyFlags != 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean setVariable(int variableId, Object variable) {
+    switch(variableId) {
+      case BR.stringValue :
+        setStringValue((java.lang.String) variable);
+        return true;
+    }
+    return false;
+  }
+
+  public void setStringValue(java.lang.String StringValue) {
+    this.mStringValue = StringValue;
+    synchronized(this) {
+      mDirtyFlags |= 0x1L;
+    }
+    notifyPropertyChanged(BR.stringValue);
+    super.requestRebind();
+  }
+  public java.lang.String getStringValue() {
+    return mStringValue;
+  }
+
+  @Override
+  protected boolean onFieldChange(int localFieldId, Object object, int fieldId) {
+    switch (localFieldId) {
+    }
+    return false;
+  }
+
+  @Override
+  protected void executeBindings() {
+    long dirtyFlags = 0;
+    synchronized(this) {
+      dirtyFlags = mDirtyFlags;
+      mDirtyFlags = 0;
+    }
+    java.lang.String stringValue = mStringValue;
+
+    if ((dirtyFlags & 0x3L) != 0) {
+    }
+    // batch finished
+    if ((dirtyFlags & 0x3L) != 0) {
+      // api target 1
+
+      android.databinding.adapters.TextViewBindingAdapter.setText(this.button, stringValue);
+    }
+  }
+  // Listener Stub Implementations
+  // callback impls
+  // dirty flag
+  private  long mDirtyFlags = 0xffffffffffffffffL;
+
+  public static ModelWithDataBindingBinding inflate(android.view.LayoutInflater inflater, android.view.ViewGroup root, boolean attachToRoot) {
+    return inflate(inflater, root, attachToRoot, android.databinding.DataBindingUtil.getDefaultComponent());
+  }
+  public static ModelWithDataBindingBinding inflate(android.view.LayoutInflater inflater, android.view.ViewGroup root, boolean attachToRoot, android.databinding.DataBindingComponent bindingComponent) {
+    return android.databinding.DataBindingUtil.<ModelWithDataBindingBinding>inflate(inflater, com.airbnb.epoxy.R.layout.model_with_data_binding, root, attachToRoot, bindingComponent);
+  }
+  public static ModelWithDataBindingBinding inflate(android.view.LayoutInflater inflater) {
+    return inflate(inflater, android.databinding.DataBindingUtil.getDefaultComponent());
+  }
+  public static ModelWithDataBindingBinding inflate(android.view.LayoutInflater inflater, android.databinding.DataBindingComponent bindingComponent) {
+    return bind(inflater.inflate(com.airbnb.epoxy.R.layout.model_with_data_binding, null, false), bindingComponent);
+  }
+  public static ModelWithDataBindingBinding bind(android.view.View view) {
+    return bind(view, android.databinding.DataBindingUtil.getDefaultComponent());
+  }
+  public static ModelWithDataBindingBinding bind(android.view.View view, android.databinding.DataBindingComponent bindingComponent) {
+    if (!"layout/model_with_data_binding_0".equals(view.getTag())) {
+      throw new RuntimeException("view tag isn't correct on view:" + view.getTag());
+    }
+    return new ModelWithDataBindingBinding(bindingComponent, view);
+  }
+    /* flag mapping
+        flag 0 (0x1L): stringValue
+        flag 1 (0x2L): null
+    flag mapping end*/
+  //end
+}
