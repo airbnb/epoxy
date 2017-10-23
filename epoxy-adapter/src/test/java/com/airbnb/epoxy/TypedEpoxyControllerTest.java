62203b5e0e3bb82931d57c98e991adab5c720ffb
Eli Hart <eli.hart@airbnb.com>
2017-10-22 18:06:26 -0700

    Change requestDelayedModelBuild to extend delay on multiple calls

--- epoxy-adapter/src/test/java/com/airbnb/epoxy/TypedEpoxyControllerTest.java
+++ epoxy-adapter/src/test/java/com/airbnb/epoxy/TypedEpoxyControllerTest.java
@@ -4,24 +4,34 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static junit.framework.Assert.assertEquals;
 
 @Config(sdk = 21, manifest = TestRunner.MANIFEST_PATH)
 @RunWith(TestRunner.class)
 public class TypedEpoxyControllerTest {
 
+  static class TestTypedController extends TypedEpoxyController<String> {
+    int numTimesBuiltModels = 0;
+
+    @Override
+    protected void buildModels(String data) {
+      assertEquals("data", data);
+      numTimesBuiltModels++;
+    }
+  }
+
   @Test
   public void setData() {
-    TypedEpoxyController<String> controller = spy(new TypedEpoxyController<String>() {
-      @Override
-      protected void buildModels(String data) {
-
-      }
-    });
+    TestTypedController controller = new TestTypedController();
 
     controller.setData("data");
+    controller.setData("data");
 
-    verify(controller).buildModels("data");
+    controller.cancelPendingModelBuild();
+
+    controller.setData("data");
+    controller.setData("data");
+
+    assertEquals(4, controller.numTimesBuiltModels);
   }
 }
