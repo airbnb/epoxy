ed62e31b92e586ec6b97df24b88ec60fed96c8a2
William Kranich <william.kranich@pelotoncycle.com>
2017-12-13 15:09:29 -0500

    Reverse studio auto-import

--- epoxy-pagingsample/src/main/java/com/airbnb/epoxy/pagingsample/DataBaseSetup.kt
+++ epoxy-pagingsample/src/main/java/com/airbnb/epoxy/pagingsample/DataBaseSetup.kt
@@ -1,16 +1,7 @@
 package com.airbnb.epoxy.pagingsample
 
-import android.arch.paging.DataSource
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.Delete
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy
-import android.arch.persistence.room.PrimaryKey
-import android.arch.persistence.room.Query
-import android.arch.persistence.room.RoomDatabase
+import android.arch.paging.*
+import android.arch.persistence.room.*
 
 @Database(entities = arrayOf(User::class), version = 1)
 abstract class PagingDatabase : RoomDatabase() {
