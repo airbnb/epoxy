9f2b59d2b1095e3a0918ea674ccb34a170bebf9f
Eli Hart <eli.hart@airbnb.com>
2018-01-28 16:05:23 -0800

    disable placeholders in paging example

--- epoxy-pagingsample/src/main/java/com/airbnb/epoxy/pagingsample/PagingSampleActivity.kt
+++ epoxy-pagingsample/src/main/java/com/airbnb/epoxy/pagingsample/PagingSampleActivity.kt
@@ -1,18 +1,23 @@
 package com.airbnb.epoxy.pagingsample
 
-import android.arch.paging.*
-import android.content.*
-import android.os.*
-import android.support.v7.app.*
-import android.support.v7.widget.*
-import android.widget.*
-import com.airbnb.epoxy.*
-import com.airbnb.epoxy.paging.*
-import android.arch.persistence.room.*;
-import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.android.*
-import org.jetbrains.anko.coroutines.experimental.*
-import java.util.concurrent.*
+import android.arch.paging.PagedList
+import android.arch.persistence.room.Room
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
+import com.airbnb.epoxy.ModelView
+import com.airbnb.epoxy.TextProp
+import com.airbnb.epoxy.paging.PagingEpoxyController
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
+import java.util.concurrent.Executor
 
 class PagingSampleActivity : AppCompatActivity() {
 
@@ -40,7 +45,7 @@ class PagingSampleActivity : AppCompatActivity() {
                 return@bg PagedList.Builder<Int, User>(
                         db.userDao().dataSource.create(),
                         PagedList.Config.Builder().run {
-                            setEnablePlaceholders(true)
+                            setEnablePlaceholders(false)
                             setPageSize(40)
                             setInitialLoadSizeHint(80)
                             setPrefetchDistance(50)
@@ -63,15 +68,11 @@ class TestController : PagingEpoxyController<User>() {
         setDebugLoggingEnabled(true)
     }
 
-    override fun buildModels(users: List<User?>) {
-        println("build ${users.size}")
-
+    override fun buildModels(users: List<User>) {
         users.forEach {
-            if (it != null) {
-                pagingView {
-                    id(it.uid)
-                    name("Id: ${it.uid}")
-                }
+            pagingView {
+                id(it.uid)
+                name("Id: ${it.uid}")
             }
         }
     }
