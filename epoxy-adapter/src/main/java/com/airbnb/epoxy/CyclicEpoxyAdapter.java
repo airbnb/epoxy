e324fb5d244830110c678a5ef79b08e4c39a83a2
Eli Hart <eli.hart@airbnb.com>
2018-02-06 21:48:50 -0800

    Cyclic adapter

---
+++ epoxy-adapter/src/main/java/com/airbnb/epoxy/CyclicEpoxyAdapter.java
@@ -0,0 +1,22 @@
+package com.airbnb.epoxy;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.ViewGroup;
+
+public class CyclicEpoxyAdapter extends RecyclerView.Adapter {
+  @Override
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(ViewHolder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+}
