1da5ea9d65dc854dca848611bdaff050b1c73b92
Martin Bonnin <martin@mbonnin.net>
2018-09-05 00:32:44 +0200

    made the test compile, not sure if they are actually used

--- kotlinsample/src/test/java/com/airbnb/epoxy/kotlinsample/ConstructorWithLambdaModel.kt
+++ kotlinsample/src/test/java/com/airbnb/epoxy/kotlinsample/ConstructorWithLambdaModel.kt
@@ -5,7 +5,7 @@ import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 
-@EpoxyModelClass(layout = R.layout.activity_kotlin_sample)
+@EpoxyModelClass(layout = R.layout.activity)
 abstract class KotlinConstructorSample(val listener: (someBool: Boolean, anotherInt: Int) -> Unit): EpoxyModelWithHolder<KotlinConstructorHolder>() {
 
 }
