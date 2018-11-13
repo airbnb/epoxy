9b6c175ed6a349c5a773bd850079dbcde40c057b
Eli Hart <eli.hart@airbnb.com>
2018-11-13 11:10:43 -0600

    Clean up and make sample work

---
+++ epoxy-sample/src/main/java/com/airbnb/epoxy/sample/models/ImageButtonModel.kt
@@ -0,0 +1,35 @@
+package com.airbnb.epoxy.sample.models
+
+import android.view.View.OnClickListener
+import android.widget.ImageView
+
+import com.airbnb.epoxy.EpoxyAttribute
+import com.airbnb.epoxy.EpoxyModelClass
+import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.airbnb.epoxy.sample.R
+import com.airbnb.epoxy.sample.models.ImageButtonModel.ImageButtonHolder
+
+import androidx.annotation.DrawableRes
+
+import com.airbnb.epoxy.EpoxyAttribute.Option.DoNotHash
+
+@EpoxyModelClass(layout = R.layout.model_image_button)
+abstract class ImageButtonModel : EpoxyModelWithHolder<ImageButtonHolder>() {
+    @EpoxyAttribute @DrawableRes var imageRes: Int = 0
+    @EpoxyAttribute(DoNotHash) var clickListener: OnClickListener? = null
+
+    override fun bind(holder: ImageButtonHolder) {
+        holder.button.setImageResource(imageRes)
+        holder.button.setOnClickListener(clickListener)
+    }
+
+    override fun unbind(holder: ImageButtonHolder) {
+        // Release resources and don't leak listeners as this view goes back to the view pool
+        holder.button.setOnClickListener(null)
+        holder.button.setImageDrawable(null)
+    }
+
+    class ImageButtonHolder : BaseEpoxyHolder() {
+        val button: ImageView by bind((R.id.button))
+    }
+}
