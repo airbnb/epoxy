60b799792ec6329b7eff16dbf8057210407a1808
AkshayChordiya <akshaychordiya2@gmail.com>
2019-10-09 15:32:25 +0200

    Add sticky header sample app

---
+++ kotlinsample/src/main/java/com/airbnb/epoxy/kotlinsample/StickyHeaderActivity.kt
@@ -0,0 +1,24 @@
+package com.airbnb.epoxy.kotlinsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.airbnb.epoxy.stickyheader.StickyHeaderLinearLayoutManager
+
+/**
+ * Shows an activity with sticky header using
+ * [StickyHeaderLinearLayoutManager]
+ */
+class StickyHeaderActivity : AppCompatActivity() {
+    private lateinit var recyclerView: EpoxyRecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity)
+
+        recyclerView = findViewById(R.id.recycler_view)
+
+        recyclerView.layoutManager = StickyHeaderLinearLayoutManager<StickyHeaderAdapter>(this)
+        recyclerView.adapter = StickyHeaderAdapter(this)
+    }
+}
---
+++ kotlinsample/src/main/java/com/airbnb/epoxy/kotlinsample/StickyHeaderAdapter.kt
@@ -0,0 +1,43 @@
+package com.airbnb.epoxy.kotlinsample
+
+import android.content.Context
+import android.widget.Toast
+import com.airbnb.epoxy.EpoxyAdapter
+import com.airbnb.epoxy.kotlinsample.models.ItemEpoxyHolder_
+import com.airbnb.epoxy.kotlinsample.models.StickyItemEpoxyHolder
+import com.airbnb.epoxy.kotlinsample.models.StickyItemEpoxyHolder_
+import com.airbnb.epoxy.stickyheader.HasStickyHeader
+
+/**
+ * Showcases [EpoxyAdapter] with sticky header support
+ */
+class StickyHeaderAdapter(
+    private val context: Context
+) : EpoxyAdapter(), HasStickyHeader {
+
+    init {
+        enableDiffing()
+        for (i in 0 until 100) {
+            addModel(when {
+                i % 5 == 0 -> StickyItemEpoxyHolder_().apply {
+                    id("sticky-header $i")
+                    title("Sticky header $i")
+                    listener {
+                        Toast.makeText(context, "clicked", Toast.LENGTH_LONG).show()
+                    }
+                }
+                else -> ItemEpoxyHolder_().apply {
+                    id("view holder $i")
+                    title("this is a View Holder item")
+                    listener {
+                        Toast.makeText(context, "clicked", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            })
+        }
+        notifyModelsChanged()
+    }
+
+    override fun isStickyHeader(position: Int) = models[position] is StickyItemEpoxyHolder
+}
