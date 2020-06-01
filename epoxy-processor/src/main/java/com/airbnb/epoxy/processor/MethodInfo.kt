ac659233cabccdb5c75d8c4fa25cdab28a8428ab
Eli Hart <eli.hart@airbnb.com>
2020-06-01 14:07:08 -0500

    Fix duplicate layout method

--- epoxy-processor/src/main/java/com/airbnb/epoxy/processor/MethodInfo.kt
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/processor/MethodInfo.kt
@@ -11,4 +11,24 @@ data class MethodInfo(
     val varargs: Boolean,
     val isEpoxyAttribute: Boolean,
     val methodElement: ExecutableElement
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MethodInfo
+
+        if (name != other.name) return false
+        if (params != other.params) return false
+        if (varargs != other.varargs) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name?.hashCode() ?: 0
+        result = 31 * result + params.hashCode()
+        result = 31 * result + varargs.hashCode()
+        return result
+    }
+}
---
+++ epoxy-processortest/src/test/resources/AbstractEpoxyModelWithView.java
@@ -0,0 +1,17 @@
+package com.airbnb.epoxy;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.airbnb.epoxy.EpoxyModelClass;
+import com.airbnb.epoxy.EpoxyModelWithView;
+
+import androidx.annotation.NonNull;
+
+@EpoxyModelClass
+public abstract class AbstractEpoxyModelWithView extends EpoxyModelWithView<View> {
+  @Override
+  protected View buildView(@NonNull ViewGroup parent) {
+    return new View(parent.getContext());
+  }
+}
