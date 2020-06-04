6a9c20d57c2d38349b5eefa6739a18a13edfa5e7
Eli Hart <eli.hart@airbnb.com>
2020-06-04 11:21:06 -0500

    don't match on param name in method comparison

--- epoxy-processor/src/main/java/com/airbnb/epoxy/processor/MethodInfo.kt
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/processor/MethodInfo.kt
@@ -1,6 +1,7 @@
 package com.airbnb.epoxy.processor
 
 import com.squareup.javapoet.ParameterSpec
+import com.squareup.javapoet.TypeName
 import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.Modifier
 
@@ -12,6 +13,10 @@ data class MethodInfo(
     val isEpoxyAttribute: Boolean,
     val methodElement: ExecutableElement
 ) {
+    private val paramTypes: List<TypeName> get() = params.map { it.type }
+
+    // Use an equals/hashcode that matches method signature, but doesn't count non signature
+    // changes such as annotations, return type, or param names.
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -19,7 +24,7 @@ data class MethodInfo(
         other as MethodInfo
 
         if (name != other.name) return false
-        if (params != other.params) return false
+        if (paramTypes != other.paramTypes) return false
         if (varargs != other.varargs) return false
 
         return true
@@ -27,7 +32,7 @@ data class MethodInfo(
 
     override fun hashCode(): Int {
         var result = name?.hashCode() ?: 0
-        result = 31 * result + params.hashCode()
+        result = 31 * result + paramTypes.hashCode()
         result = 31 * result + varargs.hashCode()
         return result
     }
