f66121a0400307cea696d5034fa08d19c2e2ed6a
Emmanuel Boudrant <eboudrant@netflix.com>
2020-07-22 11:42:34 -0700

    Add a small doc/usage

--- epoxy-adapter/src/main/java/com/airbnb/epoxy/GroupModel.kt
+++ epoxy-adapter/src/main/java/com/airbnb/epoxy/GroupModel.kt
@@ -1,7 +1,23 @@
 package com.airbnb.epoxy
 
 /**
- * An [EpoxyModelGroup] usable in DSL.
+ * An [EpoxyModelGroup] usable in a DSL manner via the [group] extension.
+ * <p>
+ * Example:
+ * ```
+ * group {
+ *   id("photos")
+ *   layout(R.layout.photo_grid)
+ *
+ *   // add your models here, example:
+ *   for (photo in photos) {
+ *     imageView {
+ *       id(photo.id)
+ *       url(photo.url)
+ *     }
+ *   }
+ * }
+ * ```
  */
 @EpoxyModelClass
 abstract class GroupModel : EpoxyModelGroup(), ModelCollector {
