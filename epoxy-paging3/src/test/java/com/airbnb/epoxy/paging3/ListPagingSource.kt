8e2c1c4f512b084ad70ea6fd4fc08979ece55cba
Duc Le Tran <letrananhduc0311@gmail.com>
2021-02-03 18:14:00 +0700

    Add simple implementation for getRefreshKey abstract function

--- epoxy-paging3/src/test/java/com/airbnb/epoxy/paging3/ListPagingSource.kt
+++ epoxy-paging3/src/test/java/com/airbnb/epoxy/paging3/ListPagingSource.kt
@@ -1,6 +1,7 @@
 package com.airbnb.epoxy.paging3
 
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
@@ -18,6 +19,8 @@ class ListPagingSource<T : Any>(
             LoadResult.Page(data.subList(key, key + params.loadSize), null, key + params.loadSize)
         }
     }
+
+    override fun getRefreshKey(state: PagingState<Int, T>): Int? = null
 
     override val jumpingSupported: Boolean
         get() = super.jumpingSupported
