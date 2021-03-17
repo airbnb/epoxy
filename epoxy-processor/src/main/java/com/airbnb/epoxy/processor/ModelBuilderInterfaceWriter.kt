airbnb/epoxy

commit fa61cfbc4059f88a3e89644ab21b3f8d70aa7c57

Updated the package name of generated model files for interface class (#1142)
* Updated package name of interface class

* Nit

* Fix typo

Co-authored-by: Eli Hart <eli.hart@airbnb.com>

* Nit

Co-authored-by: Vinay_Gaba <vinay.gaba@airbnb.com>
Co-authored-by: Eli Hart <eli.hart@airbnb.com>

epoxy-processor/src/main/java/com/airbnb/epoxy/processor/ModelBuilderInterfaceWriter.kt
@@ -10,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap
 import javax.annotation.processing.Filer
 import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
 
 const val MODEL_BUILDER_INTERFACE_SUFFIX = "Builder"
@@ -24,7 +25,9 @@ const val MODEL_BUILDER_INTERFACE_SUFFIX = "Builder"
 class ModelBuilderInterfaceWriter(
     private val filer: Filer,
     val types: Types,
-    val asyncable: Asyncable
+    val asyncable: Asyncable,
+    val configManager: ConfigManager,
+    val elements: Elements
 ) : Asyncable by asyncable {
 
     private val viewInterfacesToGenerate = ConcurrentHashMap<ClassName, InterfaceDetails>()
@@ -156,7 +159,11 @@ class ModelBuilderInterfaceWriter(
                 }
             }
 
-            JavaFile.builder(interfaceName.packageName(), interfaceSpec)
+            val implementingViewTypeElement = details.implementingViews.firstOrNull()
+            val packageName = implementingViewTypeElement?.let {
+                configManager.getModelViewConfig(it)?.rClass?.packageName()
+            } ?: interfaceName.packageName()
+            JavaFile.builder(packageName, interfaceSpec)
                 .build()
                 .writeSynchronized(filer)
         }

epoxy-processor/src/main/java/com/airbnb/epoxy/processor/GeneratedModelWriter.kt
@@ -58,7 +58,8 @@ class GeneratedModelWriter(
     asyncable: Asyncable
 ) {
 
-    val modelInterfaceWriter = ModelBuilderInterfaceWriter(filer, types, asyncable)
+    val modelInterfaceWriter =
+        ModelBuilderInterfaceWriter(filer, types, asyncable, configManager, elements)
 
     open class BuilderHooks {
         open fun beforeFinalBuild(builder: TypeSpec.Builder) {}

commit 83a60a54fa8215a6d41b28f7b01566aae2740217

Fix for generated interfaces regression from #1142 (#1150)
* Changes

* Cleanup

* Fix indentation

* Change order of setting package and remove unnecessary code

epoxy-processor/src/main/java/com/airbnb/epoxy/processor/ModelBuilderInterfaceWriter.kt
@@ -50,7 +50,12 @@ class ModelBuilderInterfaceWriter(
             val interfaceMethods = getInterfaceMethods(modelInfo, methods, interfaceName)
 
             if (modelInfo is ModelViewInfo) {
-                modelInfo.generatedViewInterfaceNames.forEach { viewInterface ->
+                modelInfo.viewInterfaces.forEach { it ->
+                    val packageName =
+                        configManager.getModelViewConfig(modelInfo.viewElement)?.rClass?.packageName()
+                            ?: elements.getPackageOf(it).qualifiedName.toString()
+                    val viewInterface =
+                        ClassName.get(it).appendToName("Model_").setPackage(packageName)
                     addSuperinterface(viewInterface)
 
                     // Store the subset of methods common to all interface implementations so we

epoxy-processor/src/main/java/com/airbnb/epoxy/processor/ModelViewInfo.kt
@@ -49,7 +49,7 @@ class ModelViewInfo(
     private val generatedModelSuffix: String
 
     /** All interfaces the view implements that have at least one prop set by the interface. */
-    private val viewInterfaces: List<TypeElement>
+    val viewInterfaces: List<TypeElement>
 
     val viewAttributes: List<ViewAttributeInfo>
         get() = attributeInfo.filterIsInstance<ViewAttributeInfo>()
@@ -103,14 +103,6 @@ class ModelViewInfo(
         annotations.addAll(
             viewElement.buildAnnotationSpecs { DEPRECATED == it.simpleName() }
         )
-    }
-
-    /** We generate an interface on the model to represent each interface on the view.
-     * This lets models with the same view interface be grouped together. */
-    val generatedViewInterfaceNames: List<ClassName> by lazy {
-        viewInterfaces.map {
-            ClassName.get(it).appendToName("Model_")
-        }
     }
 
     fun Element.kotlinMetadata(): KotlinClassMetadata? {