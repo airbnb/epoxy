ea4f3a54ddcf4891b29baf505204a795e2a45906
aniket <aps431@nyu.edu>
2021-03-30 15:04:14 -0700

    Using epoxyInterop to show Andorid Views

---
+++ epoxy-compose/src/main/java/com/airbnb/epoxy/compose/ComposeInterop.kt
@@ -0,0 +1,14 @@
+package com.airbnb.epoxy.compose
+
+import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun epoxyInterop(callback: () -> View) {
+    AndroidView(
+        factory = { _ ->
+            callback()
+        }
+    )
+}
--- epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
+++ epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
@@ -1,24 +1,22 @@
 package com.airbnb.epoxy.compose.sample
 
 import android.os.Bundle
-import android.view.Surface
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.epoxy.compose.*
 import com.airbnb.epoxy.compose.sample.ui.theme.EpoxyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EpoxyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            epoxyInterop{
+                TextView(this).apply {
+                    text = "Android Widget TextView"
                 }
             }
         }
