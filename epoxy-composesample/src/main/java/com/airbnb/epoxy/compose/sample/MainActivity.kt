19d94a69b16e2b16a05bbb66ccbf9d9453f010b2
aniket <aps431@nyu.edu>
2021-03-31 22:20:19 -0700

    Using epoxy in composeinterop

--- epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
+++ epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
@@ -1,37 +1,32 @@
 package com.airbnb.epoxy.compose.sample
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.epoxy.ModelProp
+import com.airbnb.epoxy.ModelView
 import com.airbnb.epoxy.compose.*
+import com.airbnb.epoxy.compose.sample.epoxyviews.HeaderView
 import com.airbnb.epoxy.compose.sample.ui.theme.EpoxyTheme
+import com.airbnb.paris.extensions.addDefault
+import com.airbnb.paris.extensions.style
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EpoxyInterop {
-                TextView(this).apply {
-                    text = "Android Widget TextView"
+                HeaderView(this).apply {
+                    setTitle("testing")
+                    setCaption("Caption measuring things")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EpoxyTheme {
-        Greeting("Android")
     }
 }
---
+++ epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/epoxyviews/HeaderView.kt
@@ -0,0 +1,49 @@
+package com.airbnb.epoxy.compose.sample.epoxyviews
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.airbnb.epoxy.ModelProp
+import com.airbnb.epoxy.ModelView
+import com.airbnb.epoxy.ModelProp.Option
+
+import com.airbnb.epoxy.TextProp
+import com.airbnb.epoxy.compose.sample.R
+import com.airbnb.paris.annotations.Style
+import com.airbnb.paris.annotations.Styleable
+import com.airbnb.paris.extensions.headerViewStyle
+import com.airbnb.paris.extensions.layoutHeight
+import com.airbnb.paris.extensions.layoutWidth
+
+@Styleable // Dynamic styling via the Paris library
+@ModelView
+class HeaderView(context: Context?) : LinearLayout(context) {
+    private var title: TextView? = null
+    private var caption: TextView? = null
+
+    init {
+        setOrientation(VERTICAL)
+        inflate(getContext(), R.layout.header_view, this)
+        title = findViewById(R.id.title_text)
+        caption = findViewById(R.id.caption_text)
+    }
+
+    @TextProp(defaultRes = R.string.app_name)
+    fun setTitle(title: CharSequence?) {
+        this.title?.setText(title)
+    }
+
+    @ModelProp(options = [Option.GenerateStringOverloads])
+    fun setCaption(caption: CharSequence?) {
+        this.caption?.setText(caption)
+    }
+
+    companion object {
+        @Style(isDefault = true)
+        val headerStyle = headerViewStyle {
+            layoutWidth(ViewGroup.LayoutParams.MATCH_PARENT)
+            layoutHeight(ViewGroup.LayoutParams.WRAP_CONTENT)
+        }
+    }
+}
