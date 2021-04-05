fdb69e2f533ba235003df7b842218527d8dce1c6
aniket <aps431@nyu.edu>
2021-04-05 00:27:32 -0700

    Adding composable function in epoxyRecyclerView

--- epoxy-compose/src/main/java/com/airbnb/epoxy/compose/ComposeInterop.kt
+++ epoxy-compose/src/main/java/com/airbnb/epoxy/compose/ComposeInterop.kt
@@ -1,8 +1,12 @@
 package com.airbnb.epoxy.compose
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.viewinterop.AndroidView
+import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyModelWithView
 
 @Composable
 fun EpoxyInterop(callback: () -> View) {
@@ -11,4 +15,21 @@ fun EpoxyInterop(callback: () -> View) {
             callback()
         }
     )
+}
+
+class ComposeEpoxyModel(
+    private val composeFunction: @Composable () -> Unit
+) : EpoxyModelWithView<ComposeView>() {
+    override fun buildView(parent: ViewGroup): ComposeView = ComposeView(parent.context).apply {
+        setContent(composeFunction)
+    }
+}
+
+fun EpoxyController.composableInterop(
+    id: String,
+    composeFunction: @Composable () -> Unit
+) {
+    add(ComposeEpoxyModel(composeFunction).apply {
+        id(id)
+    })
 }
--- epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
+++ epoxy-composesample/src/main/java/com/airbnb/epoxy/compose/sample/MainActivity.kt
@@ -1,32 +1,74 @@
 package com.airbnb.epoxy.compose.sample
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.airbnb.epoxy.ModelProp
-import com.airbnb.epoxy.ModelView
-import com.airbnb.epoxy.compose.*
-import com.airbnb.epoxy.compose.sample.epoxyviews.HeaderView
-import com.airbnb.epoxy.compose.sample.ui.theme.EpoxyTheme
-import com.airbnb.paris.extensions.addDefault
-import com.airbnb.paris.extensions.style
+import androidx.compose.ui.unit.dp
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.airbnb.epoxy.compose.composableInterop
+import com.airbnb.epoxy.compose.sample.epoxyviews.headerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            EpoxyInterop {
-                HeaderView(this).apply {
-                    setTitle("testing")
-                    setCaption("Caption measuring things")
-                }
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: EpoxyRecyclerView = findViewById(R.id.interop_recycler_view)
+
+        recyclerView.withModels {
+            headerView {
+                id("header")
+                title("Testing Composable in Epoxy")
             }
+
+            composableInterop(
+                id = "compose_text",
+                composeFunction = {
+                    ShowCaption(
+                        modifier =
+                        Modifier.background(color = Color.Blue)
+                            .padding(16.dp)
+                    )
+                }
+            )
+
+            composableInterop(
+                id = "news",
+                composeFunction = { NewsStory() }
+            )
         }
+    }
+}
+
+@Composable
+@Preview
+fun ShowCaption(modifier: Modifier = Modifier) {
+    Text(
+        text = "Caption coming from composable",
+        color = Color.Yellow,
+        modifier = modifier
+    )
+}
+
+@Composable
+@Preview
+fun NewsStory() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null
+        )
+        Text("A day in Shark fin Cove")
+        Text("Davenport, California")
+        Text("December 2021")
     }
 }
