bf2cb8692a7726504f0b296e89e1dbf1b72bdc4b
Peter Elliott <peter.elliott@airbnb.com>
2021-04-14 20:16:00 -0600

    Can't use ComponentActivity directly with the current dependency version

--- epoxy-integrationtest/src/main/java/com/airbnb/epoxy/integrationtest/TestActivity.kt
+++ epoxy-integrationtest/src/main/java/com/airbnb/epoxy/integrationtest/TestActivity.kt
@@ -1,8 +1,8 @@
 package com.airbnb.epoxy.integrationtest
 
-import androidx.core.app.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Empty activity used for view testing.
  */
-class TestActivity : ComponentActivity()
+class TestActivity : AppCompatActivity()
