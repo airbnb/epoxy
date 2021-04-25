012336b587f99ceb18e5a92a70b79b998765dba1
Peter Elliott <peter.elliott@airbnb.com>
2021-04-25 10:26:15 -0600

    Doc cleanup and refactors for line length

--- epoxy-viewbinder/src/main/java/com/airbnb/epoxy/EpoxyViewBinder.kt
+++ epoxy-viewbinder/src/main/java/com/airbnb/epoxy/EpoxyViewBinder.kt
@@ -21,7 +21,8 @@ import com.airbnb.epoxy.viewbinder.R
  *
  * This is helpful in two cases:
  * 1. You want to dynamically insert a view (of unknown or dynamic type) into a layout
- * 2. You have a predefined view in a layout and you want to update it's data functionally with an EpoxyModel
+ * 2. You have a predefined view in a layout and you want to update it's data functionally with an
+ * EpoxyModel
  */
 class EpoxyViewBinder : EpoxyController() {
 
@@ -34,17 +35,20 @@ class EpoxyViewBinder : EpoxyController() {
     }
 
     internal override fun addInternal(modelToAdd: EpoxyModel<*>) {
-        require(tempModel == null) { "A model was already added to the EpoxyController. Only one should be added. " }
+        require(tempModel == null) {
+            "A model was already added to the EpoxyController. Only one should be added. "
+        }
         interceptor?.intercept(listOf(modelToAdd))
         // Don't call super to avoid epoxy complaining that we are not in a buildModels call
         tempModel = modelToAdd
     }
 
-    // This needs to be hardcoded to prevent super from crashing, since we aren't actually in model building mode
+    // This needs to be hardcoded to prevent super from crashing, since we aren't actually in model
+    // building mode
     internal override fun isModelAddedMultipleTimes(model: EpoxyModel<*>?) = false
 
     internal override fun addAfterInterceptorCallback(callback: ModelInterceptorCallback) {
-        // no op
+        // No-op
     }
 
     override fun buildModels() {
@@ -52,14 +56,14 @@ class EpoxyViewBinder : EpoxyController() {
     }
 
     /**
-     * Bind the model to the view. If the model is null the view will be hidden.
-     *
-     * If the view has previously been bound to another model a partial bind will be done to only update the props that have changed.
+     * Bind the model to the view.
+     * - If the model is null, the view will be hidden.
+     * - If the view has previously been bound to another model, a partial bind will be done to only
+     * update the props that have changed.
      */
     fun <T : View> bind(view: T, model: EpoxyModel<T>?) {
-
         val newModel = model ?: run {
-            // No model added, so we hide the view
+            // No model added, so the view is hidden
             view.isVisible = false
             return
         }
@@ -67,43 +71,52 @@ class EpoxyViewBinder : EpoxyController() {
 
         val existingHolder = view.viewHolder
 
-        val viewHolder = if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
-            EpoxyViewHolder(view.parent, view, false)
-        } else {
-            existingHolder
-        }
+        val viewHolder =
+            if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
+                EpoxyViewHolder(view.parent, view, false)
+            } else {
+                existingHolder
+            }
 
         bind(viewHolder, newModel, existingHolder?.model)
     }
 
     /**
-     * Replaces an existing view if it exists, else create a new view. This is similar to [replaceView] but it does not add the view to the
-     * parent [ViewGroup]. This allows for custom layout handling (e.g. adding constraints for a [androidx.constraintlayout.widget.ConstraintLayout].
+     * Replaces an existing view if it exists, else creates a new view. This is similar to
+     * [replaceView] but it does not add the view to the parent [ViewGroup]. This allows for custom
+     * layout handling (e.g. adding constraints for a
+     * [androidx.constraintlayout.widget.ConstraintLayout]).
      *
-     * If the previous view is the same view type then it is reused.
-     * The new view is set to have the same id as the old view if it exists. If not then a new ID is generated.
+     * If the previous view is the same view type then it is reused. The new view is set to have the
+     * same ID as the old view if it exists. If not then a new ID is generated.
      *
      * @return the [View] the model has been bound to.
      */
-    fun replaceOrCreateView(parentView: ViewGroup, previousView: View?, model: EpoxyModel<*>): View {
+    fun replaceOrCreateView(
+        parentView: ViewGroup,
+        previousView: View?,
+        model: EpoxyModel<*>
+    ): View {
         val existingHolder = previousView?.viewHolder
 
-        val viewHolder = if (existingHolder == null || !model.hasSameViewType(existingHolder.model)) {
-            val newView = model.buildView(parentView)
-            newView.id = previousView?.id ?: ViewCompat.generateViewId()
-            parentView.addView(newView)
+        val viewHolder =
+            if (existingHolder == null || !model.hasSameViewType(existingHolder.model)) {
+                val newView = model.buildView(parentView)
+                newView.id = previousView?.id ?: ViewCompat.generateViewId()
+                parentView.addView(newView)
 
-            EpoxyViewHolder(parentView, newView, false)
-        } else {
-            existingHolder
-        }
+                EpoxyViewHolder(parentView, newView, false)
+            } else {
+                existingHolder
+            }
 
         bind(viewHolder, model, null)
         return viewHolder.itemView
     }
 
     /**
-     * Similar to [replaceView] but the model is provided via a lambda that adds it to an EpoxyController.
+     * Similar to [replaceView] but the model is provided via a lambda that adds it to an
+     * [EpoxyController].
      */
     fun replaceView(previousView: View, modelProvider: EpoxyController.() -> Unit): View {
         @Suppress("UNUSED_EXPRESSION")
@@ -115,34 +128,34 @@ class EpoxyViewBinder : EpoxyController() {
     }
 
     /**
-     * Creates a view for the model and swaps it in place in the parent for the previous view.
-     * If the previous view is the same view type then it is reused.
-     * If the model is null the view is hidden.
-     * The new view is set to have the same id as the old view.
+     * Creates a view for the model and swaps it in place in the parent of the previous view.
+     * - If the previous view is the same view type, it is reused.
+     * - If the model is null, the view is hidden.
+     * - The new view is set to have the same ID as the old view.
      */
     fun replaceView(previousView: View, model: EpoxyModel<*>?): View {
-
         val newModel = model ?: run {
-            // No model added, so we hide the view
+            // No model added, so the view is hidden
             previousView.isVisible = false
             return previousView
         }
 
         val existingHolder = previousView.viewHolder
 
-        val viewHolder = if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
-            val parent = previousView.parent as ViewGroup
-            val newView = newModel.buildView(parent)
-            newView.id = previousView.id
+        val viewHolder =
+            if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
+                val parent = previousView.parent as ViewGroup
+                val newView = newModel.buildView(parent)
+                newView.id = previousView.id
 
-            val index = parent.indexOfChild(previousView)
-            parent.removeViewInLayout(previousView)
-            parent.addView(newView, index, previousView.layoutParams)
+                val index = parent.indexOfChild(previousView)
+                parent.removeViewInLayout(previousView)
+                parent.addView(newView, index, previousView.layoutParams)
 
-            EpoxyViewHolder(parent, newView, false)
-        } else {
-            existingHolder
-        }
+                EpoxyViewHolder(parent, newView, false)
+            } else {
+                existingHolder
+            }
 
         val newView = viewHolder.itemView.apply {
             isVisible = true
@@ -154,17 +167,19 @@ class EpoxyViewBinder : EpoxyController() {
     }
 
     /**
-     * Takes a EpoxyModel added by the lambda, and creates a view for it in the given container.
-     * The container should be empty, and the view will be added to it and bound to the model.
-     * If the container was previously bound to another model the existing view will be reused and updated if necessary.
+     * Takes an [EpoxyModel] added by the lambda, and creates a view for it in the given container.
+     * The container should be empty, and the view will be added to it and bound to the model. If
+     * the container was previously bound to another model, the existing view will be reused and
+     * updated if necessary.
      *
-     * @param modelProvider This lambda should be used to add a model to the EpoxyController receiver. If no model is added the container will be cleared.
+     * @param modelProvider this lambda should be used to add a model to the [EpoxyController]
+     * receiver. If no model is added the container will be cleared.
      */
     fun insertInto(container: ViewGroup, modelProvider: EpoxyController.() -> Unit) {
         require(container.childCount <= 1) { "Container cannot have more than one child" }
 
-        // This lambda should add a model to the EpoxyController, which will end up calling "addInternal" and set tempModel
-        @Suppress("UNUSED_EXPRESSION")
+        // This lambda should add a model to the EpoxyController, which will end up calling
+        // "addInternal" and set tempModel
         modelProvider()
 
         val newModel = tempModel ?: run {
@@ -175,29 +190,22 @@ class EpoxyViewBinder : EpoxyController() {
         val existingView: View? = container.getChildAt(0)
         val existingHolder = existingView?.viewHolder
 
-        val viewHolder = if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
-            container.removeAllViews()
-            val view = newModel.buildView(container)
-            container.addView(view)
-            EpoxyViewHolder(container, view, false)
-        } else {
-            existingHolder
-        }
+        val viewHolder =
+            if (existingHolder == null || !newModel.hasSameViewType(existingHolder.model)) {
+                container.removeAllViews()
+                val view = newModel.buildView(container)
+                container.addView(view)
+                EpoxyViewHolder(container, view, false)
+            } else {
+                existingHolder
+            }
 
         bind(viewHolder, newModel, existingHolder?.model)
         tempModel = null
     }
 
-    private fun bind(viewHolder: EpoxyViewHolder, newModel: EpoxyModel<*>, existingModel: EpoxyModel<*>?) {
-        if (existingModel != newModel) {
-            viewHolder.bind(newModel, existingModel, emptyList(), 0)
-            viewHolder.itemView.viewHolder = viewHolder
-        }
-    }
-
     /**
-     * Unbinds any model that is currently bound to this view.
-     * If no model is bound this is a no-op.
+     * Unbinds any model that is currently bound to this view. If no model is bound this is a no-op.
      */
     fun unbind(view: View) {
         val viewHolder = view.viewHolder ?: return
@@ -205,7 +213,19 @@ class EpoxyViewBinder : EpoxyController() {
         view.viewHolder = null
     }
 
-    private fun EpoxyModel<*>.hasSameViewType(model: EpoxyModel<*>): Boolean = ViewTypeManager.getViewType(this) == ViewTypeManager.getViewType(model)
+    private fun bind(
+        viewHolder: EpoxyViewHolder,
+        newModel: EpoxyModel<*>,
+        existingModel: EpoxyModel<*>?
+    ) {
+        if (existingModel != newModel) {
+            viewHolder.bind(newModel, existingModel, emptyList(), 0)
+            viewHolder.itemView.viewHolder = viewHolder
+        }
+    }
+
+    private fun EpoxyModel<*>.hasSameViewType(model: EpoxyModel<*>): Boolean =
+        ViewTypeManager.getViewType(this) == ViewTypeManager.getViewType(model)
 }
 
 /**
@@ -260,9 +280,8 @@ fun ViewGroup.epoxyView(
 /**
  * Shortcut for creating a [LifecycleAwareEpoxyViewBinder] in a lazy way.
  *
- * If the returned view binder is null it means that the view could not be found.
- *
  * @param viewId resource ID for the view to replace. This should be an [EpoxyViewStub].
+ * @return a view binder or null if a view with the [viewId] could not be found.
  */
 fun ComponentActivity.optionalEpoxyView(
     @IdRes viewId: Int,
@@ -281,9 +300,8 @@ fun ComponentActivity.optionalEpoxyView(
 /**
  * Shortcut for creating a [LifecycleAwareEpoxyViewBinder] in a lazy way.
  *
- * If the returned view binder is null it means that the view could not be found.
- *
  * @param viewId resource ID for the view to replace. This should be an [EpoxyViewStub].
+ * @return a view binder or null if a view with the [viewId] could not be found.
  */
 fun Fragment.optionalEpoxyView(
     @IdRes viewId: Int,
@@ -302,9 +320,8 @@ fun Fragment.optionalEpoxyView(
 /**
  * Shortcut for creating a [LifecycleAwareEpoxyViewBinder] in a lazy way.
  *
- * If the returned view binder is null it means that the view could not be found.
- *
  * @param viewId resource ID for the view to replace. This should be an [EpoxyViewStub].
+ * @return a view binder or null if a view with the [viewId] could not be found.
  */
 fun ViewGroup.optionalEpoxyView(
     @IdRes viewId: Int,
@@ -360,13 +377,20 @@ private fun ViewGroup.epoxyViewInternal(
 ).apply(initializer)
 
 /**
- * This class uses an epoxy model to update a view. The view reference is cleared when the fragment is stopped.
- * Call [invalidate] to have the model rebuilt and rebound to the view.
+ * This class uses an epoxy model to update a view. The view reference is cleared when the fragment
+ * is stopped. Call [invalidate] to have the model rebuilt and rebound to the view.
  *
- * @param viewId The id of the view stub that the Epoxy view should replace. This tells the ViewBinder where in the
- * layout to put the view. This should correspond to a view of type [EpoxyViewStub].
- * @param modelProvider A lambda for building the EpoxyModel. It expects a single model to be added to the controller
- * receiver. If no model is added the view will be hidden.
+ * @param rootView a lambda returning the parent [ViewGroup] that will be used to search for the
+ * [viewId].
+ * @param viewId the ID of the view stub that the Epoxy view should replace. This tells the
+ * view binder where in the layout to put the view. This should correspond to a view of type
+ * [EpoxyViewStub] for state restoration reasons.
+ * @param useVisibilityTracking true to get visibility callbacks using a partial impression
+ * percentage threshold of 100%, false to not track view visibility. See
+ * [EpoxyViewBinderVisibilityTracker] for more information.
+ * @param fallbackToNameLookup
+ * @param modelProvider a lambda for building the [EpoxyModel]. It expects a single model to be
+ * added to the controller receiver. If no model is added the view will be hidden.
  */
 class LifecycleAwareEpoxyViewBinder(
     private val lifecycleOwner: LifecycleOwner,
@@ -394,7 +418,7 @@ class LifecycleAwareEpoxyViewBinder(
                         "View could not be found, fallbackToNameLookup: $fallbackToNameLookup," +
                             " view id name: ${nonNullRootView.resources.getResourceEntryName(viewId)}"
                     )
-                // There's not yet a way to enforce EpoxyViewStub usage so notify in Bugsnag so we can manually fix.
+                // Propagate an error if a non EpoxyViewStub is used
                 if (lazyView !is EpoxyViewStub) {
                     viewBinder.onExceptionSwallowed(
                         IllegalStateException(
@@ -415,10 +439,12 @@ class LifecycleAwareEpoxyViewBinder(
             return lazyView!!
         }
 
+    /** Adds an [EpoxyController.Interceptor] to the [EpoxyViewBinder]. **/
     fun addInterceptor(interceptor: EpoxyController.Interceptor) {
         viewBinder.addInterceptor(interceptor)
     }
 
+    /** Replaces the [View] with the model produced by the [modelProvider] lambda. */
     fun invalidate() {
         lazyView = viewBinder.replaceView(view, modelProvider).also {
             if (useVisibilityTracking) {
@@ -427,6 +453,10 @@ class LifecycleAwareEpoxyViewBinder(
         }
     }
 
+    /**
+     * Unbinds the [EpoxyViewBinder]. It is normally not necessary to call this as this object
+     * registers itself with the provided [lifecycleOwner].
+     */
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onViewDestroyed() {
         lazyView?.let { viewBinder.unbind(it) }
@@ -437,8 +467,20 @@ class LifecycleAwareEpoxyViewBinder(
     }
 }
 
-inline fun <reified V : View> View.maybeFindViewByIdName(id: Int, fallbackToNameLookup: Boolean): V? =
-    findViewById<V>(id) ?: run {
+/**
+ * Finds a view with the resource ID using [ViewGroup.findViewById]. Should a view _not_ be found
+ * and the [fallbackToNameLookup] is true, then this recursively iterates through all children and
+ * tries to find a match based on [View.idName]. This is useful if using dynamic features. For
+ * example, if there is a layout provided by a dynamic feature module with an ID for a view, and a
+ * module in the base apk that also uses that same view ID, the ID resource will be different but
+ * the name will be the same.
+ */
+@PublishedApi
+internal inline fun <reified V : View> View.maybeFindViewByIdName(
+    id: Int,
+    fallbackToNameLookup: Boolean
+): V? =
+    findViewById(id) ?: run {
         if (!fallbackToNameLookup || id == -1) return@run null
 
         try {
@@ -448,13 +490,14 @@ inline fun <reified V : View> View.maybeFindViewByIdName(id: Int, fallbackToName
 //            throwOrNotify("Id not found in ${this::class}, fallbackToNameLookup: $fallbackToNameLookup, error message: ${e.localizedMessage}")
             null
         }?.let { idName ->
-            findViewByIdName<V>(this, idName)
+            findViewByIdName(this, idName)
         }
     }
+
 @PublishedApi
 internal inline fun <reified V : View> findViewByIdName(view: View, idName: String): V? {
-    // The view instance check is not necessary but is done to avoid looking up id name for all views
-    // as an optimization
+    // The view instance check is not necessary but is done to avoid looking up id name for all
+    // views as an optimization
     if (view is V && view.idName == idName) return view
 
     if (view is ViewGroup) {
@@ -465,6 +508,10 @@ internal inline fun <reified V : View> findViewByIdName(view: View, idName: Stri
     return null
 }
 
+/**
+ * Use [Resources.getResourceEntryName] to get the name of the ID from the resource ID. Returns
+ * null if the View's ID is -1 or does not exist.
+ */
 @PublishedApi
 internal val View.idName: String?
     get() = try {
@@ -476,7 +523,8 @@ internal val View.idName: String?
 /**
  * Returns every child view nested at all layers inside this ViewGroup.
  */
-val ViewGroup.allRecursiveChildren: Sequence<View>
+@PublishedApi
+internal val ViewGroup.allRecursiveChildren: Sequence<View>
     get() {
         return children.flatMap {
             sequenceOf(it) + if (it is ViewGroup) it.allRecursiveChildren else emptySequence()
