9a47e6245e5e45e6c5ec25e10222ce6908e88462
Chris Jenkins <chris.mark.jenkins@gmail.com>
2021-11-19 11:38:38 -0700

    fix FooterView.kt

--- epoxy-kspsample/src/main/java/com/airbnb/epoxy/ksp/sample/epoxyviews/FooterView.kt
+++ epoxy-kspsample/src/main/java/com/airbnb/epoxy/ksp/sample/epoxyviews/FooterView.kt
@@ -1,21 +1,21 @@
 package com.airbnb.epoxy.ksp.sample.epoxyviews
 
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.ksp.sample.R
 
 @EpoxyModelClass
-abstract class FooterView : EpoxyModel<ConstraintLayout>() {
+abstract class FooterView : EpoxyModel<LinearLayout>() {
 
     @EpoxyAttribute
     lateinit var text: String
 
     override fun getDefaultLayout(): Int = R.layout.footer_view
 
-    override fun bind(view: ConstraintLayout) {
+    override fun bind(view: LinearLayout) {
         super.bind(view)
         view.findViewById<TextView>(R.id.footer_text).text = text
     }
