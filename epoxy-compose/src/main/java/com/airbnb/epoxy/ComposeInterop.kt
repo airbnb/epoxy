bb6009029e8daa395b17e0134423bece8ac45981
Vinay Gaba <vinaygaba@gmail.com>
2022-08-08 17:44:42 -0700

    Fix ktlint issue

--- epoxy-compose/src/main/java/com/airbnb/epoxy/ComposeInterop.kt
+++ epoxy-compose/src/main/java/com/airbnb/epoxy/ComposeInterop.kt
@@ -7,7 +7,6 @@ import android.widget.FrameLayout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.viewinterop.AndroidView
 
 /**
